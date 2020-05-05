77d4097367391ae4e292f7f1037e570267d15739
Art Willems <darthmundus038@gmail.com>
2020-05-05 13:18:45 +0200

    transmogrifier update

--- Transmogrifier.java
+++ Transmogrifier.java
@@ -1,4 +1,7 @@
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 
 /**
  * A class to transform the state of an AlchemicIngredient and with it its
@@ -102,6 +105,39 @@ public class Transmogrifier extends Device{
 		return newUnit; 
 	}
 	
+	/**
+	 * Makes a map of all the units for liquids and their conversion numbers when going from liquid to powder.
+	 * 
+	 * @return The map with the liquid units as keys and the conversion numbers as values. 
+	 */
+	
+	private Map<String, Integer> liquidConversion(){
+		Map<String, Integer> liquid = new HashMap<String, Integer>();
+		liquid.put("drop", 6/8); 
+		liquid.put("vial", 5/7); 
+		liquid.put("bottle", 15/42); 
+		liquid.put("jug", 105/126); 
+		liquid.put("barrel", 1);
+		liquid.put("storeroom", 1); 
+		return liquid; 
+	}
+	
+	/**
+	 * Makes a map with all the units for powders as keys and conversion numbers when going from powder to liquid, as keys
+	 * 
+	 * @return The map with the powder units as keys and the conversion numbers as values. 
+	 */
+	
+	private Map<String, Integer> powderConversion(){
+		Map<String, Integer> powder = new HashMap<String, Integer>();
+		powder.put("pinch", 8/6); 
+		powder.put("sachet", 7/5); 
+		powder.put("box", 42/15); 
+		powder.put("sack", 126/105); 
+		powder.put("chest", 1);
+		powder.put("storeroom", 1); 
+		return powder; 
+	}
 	
 	/**
 	 * Change the quantity of the the transmogrified ingredient based on its new state and its new 
@@ -115,12 +151,43 @@ public class Transmogrifier extends Device{
 	
 	private int changeQuantity(AlchemicIngredient ingredient) {
 		int newQuantity = 0; 
-		int quantityBeforeTransmog = ingredient.getQuantityInSpoons();
-		
+		int quantityBfrTransmog = ingredient.getQuantity(); 
+		if(ingredient.getState() == "Liquid") {
+			Map<String, Integer> liquidConversion = liquidConversion(); 
+			for(Map.Entry<String, Integer> entry : liquidConversion.entrySet()) {
+				if(ingredient.getUnit() == entry.getKey()) {
+					newQuantity = quantityBfrTransmog * entry.getValue(); 
+				}
+				
+			}
+		}
+		Map<String, Integer> powderConversion = powderConversion();
+		for(Map.Entry<String, Integer> entry : powderConversion.entrySet()) {
+			if(ingredient.getUnit() == entry.getKey()) {
+				newQuantity = quantityBfrTransmog * entry.getValue(); 
+			}
+		}
 		return newQuantity; 
 	}
 	
-	
+	/**
+	 * Initialize the transmogrified element as a new AlchemicIngredient
+	 * 
+	 * @param ingredient
+	 * 		  The AlchemicIngredient that has to be transmogrified
+	 * @return the transmogrified of the original AlchemicIngredient
+	 */
+
+	private AlchemicIngredient setIngredient(AlchemicIngredient ingredient) {
+		int newQuant = changeQuantity(ingredient);
+		String newUnit = changeUnit(ingredient); 
+		String newState = changeState(ingredient); 
+		List<IngredientType> type = ingredient.getIngredientTypeList(); 
+		long hotness = ingredient.getHotness();
+		long coldness = ingredient.getColdness(); 
+		AlchemicIngredient transmogrified = new AlchemicIngredient(newQuant, newUnit, type, hotness, coldness, newState); 
+		return transmogrified; 
+	}
 	
 	/**
 	 * Transmogrify the ingredient in this laboratory. 
@@ -131,34 +198,18 @@ public class Transmogrifier extends Device{
 	 */
 	
 	public void changeIngredient(AlchemicIngredient ingr) {
+		List<AlchemicIngredient> ingredientsInLab = this.getLaboratory().getIngredients(); 
+		if(ingredientsInLab.contains(ingr)) {
+			ingredientsInLab.remove(ingr); 
+			AlchemicIngredient transmogrified = setIngredient(ingr); 
+			addTransmogrifiedToLabo(ingredientsInLab, transmogrified); 
+			
+		}
+		/**throw exception*/
 		
-		int newQuant = this.changeQuantity(ingr); 
-		String newUnit = this.changeUnit(ingr); 
-		IngredientType type = ingr.getIngredientType(); 
-		long hotness = ingr.getHotness(); 
-		long coldness = ingr.getColdness(); 
-		String newState = this.changeState(ingr); 
-		AlchemicIngredient transmogrifiedIngredient = new AlchemicIngredient(newQuant, newUnit, type, hotness, coldness, newState);
-		this.destroyOldIngredient(ingr);
-		this.addTransmogrifiedToLabo(transmogrifiedIngredient);
 	}
 	
-	/**
-	 * Remove the old ingredient from the list of ingredients in the laboratory 
-	 * to which the transmogrifier belongs
-	 * NOTE: work in progress
-	 * 
-	 * @param ingr
-	 * 		  The original ingredient that has to be removed. 
-	 * 		 
-	 */
-	private List<AlchemicIngredient> destroyOldIngredient(AlchemicIngredient ingr) {
-		Laboratory labo = this.getLaboratory(); 
-		List<AlchemicIngredient> oldIngredients = labo.getIngredients();
-		oldIngredients.remove(ingr); 
-		return oldIngredients; 
-	}
-	
+
 	/**
 	 * Add the transmogrified ingredient to a the list of ingredients in the laboratory. 
 	 * 
@@ -166,8 +217,11 @@ public class Transmogrifier extends Device{
 	 * 		  The transmogrified ingredient that needs to be added to the ingredients
 	 * 		  of the laboratory in which the transmogrifier is placed. 
 	 */
-	private void addTransmogrifiedToLabo(AlchemicIngredient transmogrifiedIngredient) {
-
+	
+	private void addTransmogrifiedToLabo(List<AlchemicIngredient> oldIngredients, AlchemicIngredient transmogrifiedIngredient) {
+		oldIngredients.add(transmogrifiedIngredient);
+		Laboratory lab = getLaboratory(); 
+		
 	}
 	
 
