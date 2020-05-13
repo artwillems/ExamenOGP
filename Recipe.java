8d1ec027a142f3b3a848f28a4a967c5745601477
Marie Levrau <marie.levrau@student.kuleuven.be>
2020-05-13 23:25:32 +0200

    recipe

---
+++ Recipe.java
@@ -0,0 +1,10 @@
+/**
+ * A class of recipes involving a list of operations that needs to be executed on a list of quantities of ingredients.
+ * 
+ * 
+* @author Jérôme D'hulst, Marie Levrau, Art Willems
+*/
+public class Recipe {
+	
+	public Recipe(List<> listOfOperations, List<> listOfIngredients);
+}
