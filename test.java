9a232a30ec549a48a90174b7ed9f3afa1f93523d
Marie Levrau <marie.levrau@student.kuleuven.be>
2020-05-16 16:47:34 +0200

    update

---
+++ test.java
@@ -0,0 +1,12 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		Temperature temp = new Temperature(0,20);
+		System.out.println(temp);
+		System.out.println("Hallo");
+
+	}
+
+}
