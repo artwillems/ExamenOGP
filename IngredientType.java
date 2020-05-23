fe682bb18a9f1d743f04a7744680bb3ca2f6d209
Marie Levrau <marie.levrau@student.kuleuven.be>
2020-05-23 15:59:47 +0200

    ingredienttype

--- IngredientType.java
+++ IngredientType.java
@@ -12,8 +12,6 @@ import be.kuleuven.cs.som.annotate.Raw;
  * 			| isValidName(getName())
  * @invar	Each ingredientType must have a valid state.
  * 			| isValidState(getState())
- * @invar	Each ingredientType must have a valid standard temperature.
- * 			| isValidStandardTemp(getStandardTemp())
  * 
 * @author Jérôme D'hulst, Marie Levrau, Art Willems
 */
@@ -25,21 +23,22 @@ public class IngredientType {
      **********************************************************/
 	
 	/**
-	 * Initialize a new ingredientType with given name, state and standard temperature.
+	 * Initialize a new ingredientType with given name, state, coldness and hotness.
 	 * @param 	name
 	 * 			The name of the new ingredientType.
 	 * @param 	state
 	 * 			The state of the new ingredientType.
-	 * @param	standardTemp
-	 * 			The standard temperature of the new ingredientType.
-	 * 
+	 * @param	coldness
+	 * 			The standard coldness of the new ingredientType.
+	 * @param	hotness
+	 * 			The standard hotness of the new ingredientType.
 	 * @effect  The name of the ingredient type is set to the given name.
 	 * 			If the given name is not valid, a default name is set.
 	 *          | setName(name) 
 	 * @effect  The state of the ingredient type is set to the given state.
 	 * 			If the given state is not valid, a default state is set.
 	 *          | setState(state) 
-	 * @effect  The standard temperature of the ingredient type is set to the given standard temperature.
+	 * @effect  The standard temperature of the ingredient type is set to the given coldness and hotness.
 	 * 			If the given standard temperature is not valid, a default standard temperature is set.
 	 *          | setStandardTemp(standardTemp) 	 
 	 */
@@ -55,7 +54,7 @@ public class IngredientType {
 	 * 			The name of the new ingredientType.
 	 * @param	standardTemp
 	 * 			The standard temperature of the new ingredientType.
-	 * @effect	The new ingredient type is initialized with the given name, state and standard temperature.
+	 * @effect	The new ingredient type is initialized with the given name, coldness and hotness.
 	 * 			| this(name,null,standardTemp)
 	 *  
 	 */
@@ -85,9 +84,17 @@ public class IngredientType {
      * 
      * @param  	name
      *			The name to be checked
-     * @return	True if 
-     * (name.charAt(0)).matches("[A-Z^()]+")
-     * String.valueOf(name.charAt(0))
+     * @return	True if the name contains out of one word, then the word exists out of at least three letters. 
+     * 			If the name contains more than one word, then each word exists out of at least two letters. Each word starts with
+     * 			a capital letter or special allowed character. The rest is written in lower cases. 
+     * 			|String[] splitName = name.split(" ");
+     * 			|boolean result = true;
+     * 			|if (splitName.length > 1)
+     * 			|	for i in splitName
+     * 			|		if ((splitName[i] == null) || (splitName[i].length() < 2) || (String.valueOf(splitName[i].charAt(0)).matches("[A-Z^()]+")) == false || (splitName[i].substring(1).matches("[a-z]+") == false))
+     * 			|			result == false
+     * 			|else	
+     * 			|	result == (name != null && String.valueOf(name.charAt(0)).matches("[A-Z^()]+") && name.substring(1).matches("[a-z]+") && (name.length() >= 3)  )
      */
     public static boolean isValidName(String name) {
     	String[] splitName = name.split(" ");
@@ -146,7 +153,7 @@ public class IngredientType {
      **********************************************************/
 	
 	/**
-	 * Variable referencing the name of this ingredientType.
+	 * Variable referencing the state of this ingredientType.
 	 */
 	private String state = null;
 	
@@ -229,7 +236,7 @@ public class IngredientType {
      * @param standardTemp
      * 		  The new standard temperature for this ingredientType
      */
-    @Raw @Basic 
+    @Model 
     public void setStandardTemp(long coldness, long hotness) {
     	if (isValidStandardTemp(coldness,hotness)) {
     		this.standardTemp = new Temperature(coldness,hotness);
@@ -245,7 +252,11 @@ public class IngredientType {
      * 
      * @param  	standardTemp
      *			The standard temperature to be checked
-     * @return	True if the given standard temperature is strictly higher than [0,0].
+     * @return	True if the given standard temperature is strictly higher than [0,0] and either the coldness is strictly higher than zero or the hotness is strictly higher than zero.
+     *			|if (((coldness == 0)  && (hotness == 0)) || ((coldness >0) && (hotness > 0)) || (coldness < 0) || (hotness <0))
+     *			|	result == false
+     *			|else
+     *			|	result == true
      */
     public boolean isValidStandardTemp(long coldness, long hotness) {
         if (((coldness == 0)  && (hotness == 0)) || ((coldness >0) && (hotness > 0)) || (coldness < 0) || (hotness <0)) {
@@ -257,9 +268,17 @@ public class IngredientType {
     	
     }
 
+    /**
+	 * Variable referencing the default standard temperature of this ingredientType.
+	 */
     private static final Temperature defaultStandardTemp = new Temperature(0,20);
     
 
+    /**
+     * Return the default standard temperature for a new ingredientType which is to be used when the
+     * given default standard temperature is not valid.
+     *
+     */
     @Model
     private static Temperature getDefaultStandardTemp() {
     	return defaultStandardTemp;
